1721294806cff98b79743bff0c90719b34feb69f
Michael Gold <goldmichael@gmail.com>
2017-09-14 19:49:06 -0400

    added notification scripts for all active tasks and next tasks

--- task-popup-next.sh
+++ task-popup-next.sh
@@ -3,18 +3,17 @@
 # For cron: */30 * * * * DISPLAY=:0.0 /home/User/configs/TaskNotify.sh
 # Requires linux with notify-send and canberra-gtk-play which are default on Ubuntu
 
-num=`task active | wc -l`
+num=`task +next +UNBLOCKED | wc -l`
 
 if [ $num -gt "1" ]
   then
 
     #notify-send "Active Tasks" "`task active | tail -n +3 | head -n -1`" 
     #canberra-gtk-play --file=/usr/share/sounds/gnome/default/alerts/drip.ogg
-    id_list="$(task +ACTIVE _ids)"
+    id_list="$(task +next +UNBLOCKED _ids)"
     id_list="${id_list//$'\n'/' '}"
 
-
-
+    
     IFS=' ' read -r -a ids <<< "$id_list"
     for i in "${ids[@]}"
     	do
