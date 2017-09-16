a3b407d8bd05ce63efa3681b6fea1f627ad4a339
Michael Gold <goldmichael@gmail.com>
2017-09-16 09:07:49 -0400

    refactored task-popup-next

--- task-popup-next.sh
+++ task-popup-next.sh
@@ -7,23 +7,18 @@ num=`task +next +UNBLOCKED | wc -l`
 
 if [ $num -gt "1" ]
   then
+    
+    # get tasks and parse into an array
 
-    #notify-send "Active Tasks" "`task active | tail -n +3 | head -n -1`" 
-    #canberra-gtk-play --file=/usr/share/sounds/gnome/default/alerts/drip.ogg
     id_list="$(task +next +UNBLOCKED _ids)"
     id_list="${id_list//$'\n'/' '}"
-
-    
     IFS=' ' read -r -a ids <<< "$id_list"
+    
     for i in "${ids[@]}"
-    	do
-		notify-send "<b>$(task _get $i.id) - $(task _get $i.tags)</b>\n$(task _get $i.description)\n$(task _get $i.project)"
-		sleep 0.35
-	done
-	
- 
-		
-		#notify-send "$(task _get `task +ACTIVE ids`.id) - $(task _get `task +ACTIVE ids`.description)\n$(task _get `task +ACTIVE ids`.project)"
+    do
+		    notify-send "<b>$(task _get $i.id) - $(task _get $i.tags)</b>\n$(task _get $i.description)\n$(task _get $i.project)"
+		    sleep 0.35
+	  done
   else
     notify-send "No Active Tasks"  
     #canberra-gtk-play --file=/usr/share/sounds/ubuntu/stereo/dialog-question.ogg
